2548124a5fa244ffe06b2eb01d0daee41e72848f
Daniel Meza <danielmeza.4c@gmail.com>
2021-02-01 12:16:07 -0700

    Agregar archivos de proyecto.

---
+++ PeliculasAPI/Entities/Genre.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeliculasAPI.Entities
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+    }
+}
---
+++ PeliculasAPI/repositorios/RepositorioEnMemoria.cs
@@ -0,0 +1,27 @@
+﻿using PeliculasAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeliculasAPI.repositorios
+{
+    public class RepositorioEnMemoria
+    {
+        private List<Genre> _genres;
+
+        public RepositorioEnMemoria()
+        {
+            _genres = new List<Genre>()
+            {
+                new Genre() { Id = 1, Nombre = "Comedia" },
+                new Genre() { Id = 2, Nombre = "Acción" }
+            };
+        }
+
+        public List<Genre> ObtenerTodosLosGeneros()
+        {
+            return _genres;
+        }
+    }
+}
