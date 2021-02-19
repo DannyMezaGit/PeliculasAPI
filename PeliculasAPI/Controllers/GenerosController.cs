9c7c4451d60e160d5601f9c934c1f38d24903568
Daniel Meza <danielmeza.4c@gmail.com>
2021-02-19 09:13:07 -0700

    GenerosController - Get de lista de generos - Post de genero

---
+++ PeliculasAPI/Controllers/GenerosController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PeliculasAPI.Context;
+using PeliculasAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PeliculasAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenerosController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public GenerosController (ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public async Task<ActionResult<List<Genero>>> Get()
+        {
+            return await _dbContext.Generos.ToListAsync();
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Genero genero)
+        {
+            _dbContext.Add(genero);
+            await _dbContext.SaveChangesAsync();
+            return NoContent(); // para retornar un 204
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
